6f32a46bd6619c9f9c7f2473fbfe895bad4cb5be
Hitesh3936 <Hitesharora3936@gmail.com>
2026-01-01 22:13:39 +0530

    Add group-based bidding

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const io = new Server(server, {
 });
 
 let bids = [];
+let groups = {}; // groupCode -> bids array
+
 
 app.get("/", (req, res) => {
   res.send("Bidding server is running");
@@ -58,6 +60,60 @@ io.on("connection", (socket) => {
     document.getElementById("adminBids").innerHTML = "";
   });
 
+  socket.on("createGroup", (groupCode) => {
+    if (!groups[groupCode]) {
+      groups[groupCode] = [];
+      console.log("🆕 Group created:", groupCode);
+    }
+
+    socket.join(groupCode);
+    socket.join("admins");
+
+    socket.emit("groupCreated", groupCode);
+  });
+
+  socket.on("joinGroup", (groupCode) => {
+    if (!groups[groupCode]) {
+      socket.emit("invalidGroup");
+      return;
+    }
+
+    socket.join(groupCode);
+
+    // send old bids (amount only)
+    groups[groupCode].forEach((bid) => {
+      socket.emit("bidUpdate", { amount: bid.amount });
+    });
+
+    console.log("👤 User joined group:", groupCode);
+  });
+
+  socket.on("placeBid", ({ groupCode, user, amount }) => {
+    const bid = {
+      user,
+      amount,
+      time: new Date().toLocaleTimeString(),
+    };
+
+    groups[groupCode].push(bid);
+
+    // users (anonymous)
+    io.to(groupCode).emit("bidUpdate", { amount });
+
+    // admin (full info)
+    io.to("admins").emit("adminBidUpdate", { groupCode, ...bid });
+  });
+
+  socket.on("clearGroupBids", (groupCode) => {
+    if (groups[groupCode]) {
+      groups[groupCode] = [];
+      io.to(groupCode).emit("clearBidList");
+      io.to("admins").emit("adminBidCleared", groupCode);
+    }
+  });
+
+
+
   socket.on("clearBids", () => {
     console.log("🧹 Clear bids requested");
 
@@ -76,6 +132,41 @@ function clearAllBids() {
   console.log("🧹 Clear button clicked");
   socket.emit("clearBids");
 }
+
+function createGroup() {
+  const code = document.getElementById("groupCode").value;
+  socket.emit("createGroup", code);
+}
+
+socket.on("groupCreated", (code) => {
+  alert("Group created: " + code);
+});
+
+socket.on("adminBidUpdate", (bid) => {
+  const li = document.createElement("li");
+  li.innerText = `[${bid.groupCode}] ${bid.user} → ₹${bid.amount}`;
+  document.getElementById("adminBids").appendChild(li);
+});
+
+let currentGroup = "";
+
+function joinGroup() {
+  currentGroup = document.getElementById("groupCode").value;
+  socket.emit("joinGroup", currentGroup);
+}
+
+function bid() {
+  socket.emit("placeBid", {
+    groupCode: currentGroup,
+    user: document.getElementById("name").value,
+    amount: document.getElementById("amount").value,
+  });
+}
+
+socket.on("invalidGroup", () => {
+  alert("Invalid group code");
+});
+
 
 
 
